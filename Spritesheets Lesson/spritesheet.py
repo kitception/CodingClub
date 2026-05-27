9bbb5442d365b3bcf949a9014418fcafcc138b7c
kitception <78454361+kitception@users.noreply.github.com>
2026-05-27 14:49:48 -0700

    temporary flip logic

--- Spritesheets Lesson/spritesheet.py
+++ Spritesheets Lesson/spritesheet.py
@@ -4,10 +4,11 @@ class SpriteSheet():
     def __init__(self, image):
         self.sheet = image
 
-    def get_image(self, frame, width, height, scale, colour):
+    def get_image(self, frame, width, height, scale, colour, flip):
         image = pygame.Surface((width, height)).convert_alpha()
         image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
         image = pygame.transform.scale(image, (width * scale, height * scale))
         image.set_colorkey(colour)
+        pygame.transform.flip(image, flip, False)
 
         return image
